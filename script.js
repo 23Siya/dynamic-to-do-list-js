faf63966a8d8f8eecb07a6b194ecdb3d7cbb72a5
23Siya <156370624+23Siya@users.noreply.github.com>
2025-01-24 14:18:19 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const addButton = document.getElementById();
+    const taskList= document.getElementById("task-list");
+    const taskInput= document.getElementById("task-input");
+});
