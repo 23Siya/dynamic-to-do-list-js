530963c944655fdf57dafb7de384a76043a09a34
23Siya <156370624+23Siya@users.noreply.github.com>
2025-01-26 22:44:34 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-    const addButton = document.getElementById("add-task-button");
+    const addButton = document.getElementById("add-task-btn");
     const taskList= document.getElementById("task-list");
     const taskInput= document.getElementById("task-input");
 
