a6ef1b2b6b119c05bd238e2023d97dc10056e1b1
23Siya <156370624+23Siya@users.noreply.github.com>
2025-01-26 23:17:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 document.addEventListener("DOMContentLoaded", () => {
-
+ localStorage.getItem('tasks');
     const addButton = document.getElementById("add-task-btn");
     const taskList= document.getElementById("task-list");
     const taskInput= document.getElementById("task-input");
